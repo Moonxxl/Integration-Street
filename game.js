21ef1ead165bd977f354eadecf4c81162b27f6be
Moonxxl <shazneensayed7@gmail.com>
2026-05-14 22:16:04 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const world = document.getElementById("world");
 const bg = document.getElementById("background");
 
 let x = 200;
-let speed = 30;
+let speed = 15;
 let keys = {};
 let frame = 0;
 let facingLeft = false;
